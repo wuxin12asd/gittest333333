4332bdeedc18bc155282feb61d19b5132de9668a
wuxin <1925616965@qq.com>
2022-12-13 20:18:17 +0800

    master提交6666

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello  3world!");
+        System.out.println("H5ello  3world!");
     }
 }
